14293d345eadbabf233d1bebb2290f13be61a57c
BumbleGumbek <133594859+BumbleGumbek@users.noreply.github.com>
2025-12-15 03:52:25 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,18 @@
 <?php
 
-class db{
-    protected $conn;
-    public function __construct()
+class Db 
+{
+    protected $conn; // khai báo thuộc tính biến của class
+                     // $conn biến dùng để lưu kết nối CSDL
+                     // Bảo mật kết nối
+
+    public function __construct() //Hàm khởi tạo
     {
-        $this->conn = new mysqli("localhost","root","","db_t2c4");
-        $this->conn->set_charset('utf8');
+        //Kết nối CSDL
+        $this->conn = new mysqli("localhost", "root", "", "db_t2c4");
+        
+        //Thiết lập bảng mã tiếng việt
+        $this->conn->set_charset("utf8");
     }
 }
 
